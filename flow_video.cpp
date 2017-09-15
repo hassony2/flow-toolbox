a282dd5bc416d1051e2f146c31c88313603de4b8
Yana Hasson <yana.hasson@inria.fr>
2017-09-15 09:58:33 +0200

    Refactor flow extraction

--- flow_video.cpp
+++ flow_video.cpp
@@ -353,76 +353,10 @@ static vector<double> writeFlowJpg(string name, const Mat& d_flow)
     return mm_frame;
 }
 
-int main(int argc, const char* argv[])
+int extractFlows(const string input_name, const string out_dir, const string proc_type,
+                 string output_mm, const int interval_beg, int interval_end,
+                 const bool visualize, const bool silence)
 {
-    // Parse parameters and options
-    string input_name;              // name of the video file or directory of the image sequence
-    string proc_type    = "brox";    // "farn", "brox" or "tvl1"
-    string out_dir       = "./";     // directory for the output files
-    int interval_beg      = 1;           // 1 for the beginning
-    int interval_end    = -1;       // End (-1 for uninitialized, default set below)
-    bool visualize      = 0;        // boolean for flow visualization
-    bool silence        = 1;         // Remove logs
-    string output_mm    = "";       // name of the minmax.txt file (default set below)
-
-    const char* usage = "[-h] [-p <proc_type>] [-o <out_dir>] [-b <interval_beg>] [-e <interval_end>] [-v <visualize>] [-m <output_mm>] <input_name>";
-    string help  = "\n\n\nUSAGE:\n\t"+ string(usage) +"\n\n"
-        "INPUT:\n"
-        "\t<input_name>\t \t: Path to video file or image directory (e.g. img_%04d.jpg)\n"
-        "OPTIONS:\n"
-        "-h \t \t \t \t: Display this help message\n"
-        "-p \t<proc_type>  \t[farn] \t: Processor type (farn, brox or tvl1)\n"
-        "-o \t<out_dir> \t[./] \t: Output folder containing flow images and minmax.txt\n"
-        "-b \t<interval_beg> \t[1] \t: Frame index to start (one-based indexing)\n"
-        "-e \t<interval_end> \t[last] \t: Frame index to stop\n"
-        "-v \t<visualize> \t[0] \t: Boolean for visualization of the optical flow\n"
-        "-s \t<silence> \t[0] \t: Boolean for removing logs\n"
-        "-m \t<output_mm> \t[<out_dir>/<basename(input_name)>_minmax.txt] \t: Name of the minmax file.\n"
-        "\n"
-        "Notes:\n*GPU method: Brox, CPU method: Farneback.\n"
-        "*Only <imagename>_%0xd.jpg (x any digit) is supported for image sequence input.\n\n\n";
-// brox cpu
-//fourcc check for image sequence detection, but not sure
-    int option_char;
-    while ((option_char = getopt(argc, (char **)argv, "hp:o:b:e:m:s:v:?")) != EOF)
-    {
-        switch (option_char)
-        {  
-            case 'p': proc_type      = optarg;       break;
-            case 'o': out_dir        = optarg;       break;
-            case 'b': interval_beg   = atoi(optarg); break;
-            case 'e': interval_end   = atoi(optarg); break;
-            case 'v': visualize      = atoi(optarg); break;
-            case 's': silence        = atoi(optarg); break;
-            case 'm': output_mm      = optarg;       break;
-            case 'h': cout << help; return 0;        break;
-            case '?': fprintf(stderr, "Unknown option.\nUSAGE: %s %s\n", argv[0], usage); return -1; break;
-        }
-    }
-
-    // Retrieve the (non-option) argument
-    if ( (argc <= 1) || (argv[argc-1] == NULL) || (argv[argc-1][0] == '-') )
-    {
-        fprintf(stderr, "No input name provided.\nUSAGE: %s %s\n", argv[0], usage);
-        return -1;
-    }
-    else
-    {
-        input_name = argv[argc-1];
-    }
-
-    if(out_dir.compare("") != 0) 
-    {
-        if(out_dir[out_dir.length()-1]!= '/') { out_dir = out_dir + "/"; } //and if last char not /
-        char cmd[200];
-        sprintf(cmd, "mkdir -p %s", out_dir.c_str());
-        system(cmd);
-    }
-    if(output_mm.compare("") == 0)
-    {
-        output_mm = out_dir + "minmax.txt";
-    }
-
     // Declare useful variables
     Mat frame0, frame1;
     char name[200];
@@ -434,7 +368,6 @@ int main(int argc, const char* argv[])
         if (!silence)
         {
             cout << "Extracting flow from [" << input_name << "] using GPU." << endl;
-            // Solve the bug of allocating gpu memory after cap.read
             cout << "Initialization (this may take awhile)..." << endl;
         }
         GpuMat temp = GpuMat(3, 3, CV_32FC1);
@@ -442,7 +375,7 @@ int main(int argc, const char* argv[])
         GpuMat g_frame0, g_frame1;
         GpuMat gf_frame0, gf_frame1;
         GpuMat g_flow;
-           // Create optical flow object
+        // Create optical flow object
         Ptr<cuda::BroxOpticalFlow> brox = cuda::BroxOpticalFlow::create(0.197f, 50.0f, 0.8f, 10, 77, 10);
         Ptr<cuda::OpticalFlowDual_TVL1> tvl1 = cuda::OpticalFlowDual_TVL1::create();
 
@@ -485,6 +418,7 @@ int main(int argc, const char* argv[])
                 if(!bSuccess)
                 {
                     cout << "Cannot read frame " << name << "!" << endl;
+                    return 1;
                 }
                 else
                 {
@@ -535,6 +469,7 @@ int main(int argc, const char* argv[])
         else
         {
             cout << "Video " << input_name << " cannot be opened." << endl;
+            return 1;
         }
     }
     else if(proc_type.compare("farn") == 0)
@@ -570,7 +505,11 @@ int main(int argc, const char* argv[])
             {
                 sprintf(name, "%s%05d", out_dir.c_str(), k+interval_beg-1);
                 bool bSuccess = cap.read(frame1);
-                if(!bSuccess)   { cout << "Cannot read frame " << name << "!" << endl; }
+                if(!bSuccess)   {
+                    cout << "Cannot read frame " << name << "!" << endl;
+                    return 1;
+                }
+
                 else
                 {
                     if(!silence)
@@ -610,7 +549,84 @@ int main(int argc, const char* argv[])
         else
         {
             cout << "Video " << input_name << " cannot be opened." << endl;
+            return 1;
         }
     }
-    return 0;
+}
+
+int main(int argc, const char* argv[])
+{
+    // Parse parameters and options
+    string input_name;              // name of the video file or directory of the image sequence
+    string proc_type    = "brox";    // "farn", "brox" or "tvl1"
+    string out_dir       = "./";     // directory for the output files
+    int interval_beg      = 1;           // 1 for the beginning
+    int interval_end    = -1;       // End (-1 for uninitialized, default set below)
+    bool visualize      = 0;        // boolean for flow visualization
+    bool silence        = 1;         // Remove logs
+    string output_mm    = "";       // name of the minmax.txt file (default set below)
+
+    const char* usage = "[-h] [-p <proc_type>] [-o <out_dir>] [-b <interval_beg>] [-e <interval_end>] [-v <visualize>] [-m <output_mm>] <input_name>";
+    string help  = "\n\n\nUSAGE:\n\t"+ string(usage) +"\n\n"
+        "INPUT:\n"
+        "\t<input_name>\t \t: Path to video file or image directory (e.g. img_%04d.jpg)\n"
+        "OPTIONS:\n"
+        "-h \t \t \t \t: Display this help message\n"
+        "-p \t<proc_type>  \t[farn] \t: Processor type (farn, brox or tvl1)\n"
+        "-o \t<out_dir> \t[./] \t: Output folder containing flow images and minmax.txt\n"
+        "-b \t<interval_beg> \t[1] \t: Frame index to start (one-based indexing)\n"
+        "-e \t<interval_end> \t[last] \t: Frame index to stop\n"
+        "-v \t<visualize> \t[0] \t: Boolean for visualization of the optical flow\n"
+        "-s \t<silence> \t[0] \t: Boolean for removing logs\n"
+        "-m \t<output_mm> \t[<out_dir>/<basename(input_name)>_minmax.txt] \t: Name of the minmax file.\n"
+        "\n"
+        "Notes:\n*GPU method: Brox, CPU method: Farneback.\n"
+        "*Only <imagename>_%0xd.jpg (x any digit) is supported for image sequence input.\n\n\n";
+// brox cpu
+//fourcc check for image sequence detection, but not sure
+    int option_char;
+    while ((option_char = getopt(argc, (char **)argv, "hp:o:b:e:m:s:v:?")) != EOF)
+    {
+        switch (option_char)
+        {  
+            case 'p': proc_type      = optarg;       break;
+            case 'o': out_dir        = optarg;       break;
+            case 'b': interval_beg   = atoi(optarg); break;
+            case 'e': interval_end   = atoi(optarg); break;
+            case 'v': visualize      = atoi(optarg); break;
+            case 's': silence        = atoi(optarg); break;
+            case 'm': output_mm      = optarg;       break;
+            case 'h': cout << help; return 0;        break;
+            case '?': fprintf(stderr, "Unknown option.\nUSAGE: %s %s\n", argv[0], usage); return -1; break;
+        }
+    }
+    // Retrieve the (non-option) argument
+    if ( (argc <= 1) || (argv[argc-1] == NULL) || (argv[argc-1][0] == '-') )
+    {
+        fprintf(stderr, "No input name provided.\nUSAGE: %s %s\n", argv[0], usage);
+        return -1;
+    }
+    else
+    {
+        input_name = argv[argc-1];
+    }
+
+    if(out_dir.compare("") != 0) 
+    {
+        if(out_dir[out_dir.length()-1]!= '/') { out_dir = out_dir + "/"; } //and if last char not /
+        char cmd[200];
+        sprintf(cmd, "mkdir -p %s", out_dir.c_str());
+        system(cmd);
+    }
+    if(output_mm.compare("") == 0)
+    {
+        output_mm = out_dir + "minmax.txt";
+    }
+    int extract_success;
+    extract_success = extractFlows(input_name, out_dir,
+                                   proc_type, output_mm,
+                                   interval_beg, interval_end,
+                                   visualize, silence);
+
+    return extract_success;
 }
